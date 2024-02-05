18b4f966cba92c6438fa2a1a04efaece6204704a
aman-romanov <byaromanov@gmail.com>
2024-02-05 19:11:58 +0600

    completed task_11

--- includes/processor_10.php
+++ includes/processor_10.php
@@ -12,22 +12,22 @@
         $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
         $text = $_POST['simpleinput'];
         try {
-            $conn = new PDO($dsn, $db_user, $db_pass);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO task_10 (text)
-                VALUES (:text)";
-
-            $stmt = $conn->prepare($sql);
-
-            $stmt->bindValue(':text', $text, PDO::PARAM_STR);
-
-            $stmt->execute();
-
-            redirect('/marlin/20_tasks/task_10(2).php');
         } catch (PDOException $e){
             echo $e->getMessage();
             exit;
         }
+        $conn = new PDO($dsn, $db_user, $db_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO task_10 (text)
+            VALUES (:text)";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(':text', $text, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        redirect('/marlin/20_tasks/task_10(2).php');
     }
     
 ?>
---
+++ includes/processor_11.php
@@ -0,0 +1,49 @@
+<?php 
+    session_start();
+    require 'redirect.php';
+
+    if(empty($_POST["simpleinput"])){
+        $_SESSION['error'] = 'Заполните поле';
+        redirect('/marlin/20_tasks/task_11.php');
+    } else {
+        $db_host = "localhost";
+        $db_name = "marlin_tasks";
+        $db_user = "tester";
+        $db_pass = "vOJ1Cls7Q52GTIaT";
+
+        $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
+        $text = $_POST["simpleinput"];
+        try {
+            $conn = new PDO($dsn, $db_user, $db_pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e){
+            echo $e->getMessage();
+            exit;
+        }
+        $sql = "SELECT *
+                FROM task_10
+                WHERE text = :text";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':text', $text, PDO::PARAM_STR);
+        $stmt->execute();
+        $row_num = $stmt->rowCount();
+        if($row_num <= 0){
+
+            $sql = "INSERT INTO task_10 (text)
+                VALUES (:text)";
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(':text', $text, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            redirect('/marlin/20_tasks/task_11.php');
+        }else{
+            $_SESSION['error'] = 'Запись дублируется';
+            redirect('/marlin/20_tasks/task_11.php');
+        }
+        
+    }
+    
+?>
