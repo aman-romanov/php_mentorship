268583f51c5ee78df69ae25dec2d12e29b7f2faa
aman-romanov <byaromanov@gmail.com>
2024-02-08 17:22:56 +0600

    completed task_15

---
+++ includes/processor_15.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+    require "redirect.php";
+
+    $db_host = "localhost";
+    $db_name = "marlin_tasks";
+    $db_user = "tester";
+    $db_pass = "vOJ1Cls7Q52GTIaT";
+
+    $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
+
+    try {
+        $conn = new PDO($dsn, $db_user, $db_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e){
+        echo $e->getMessage();
+        exit;
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $sql = "SELECT *
+            FROM task_12
+            WHERE email = :email
+            AND password = :password";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+        if($stmt->execute()){
+
+            $_SESSION['is_logged_in'] = true;
+            redirect('/marlin/20_tasks/task_15.php');
+        }else{
+            $_SESSION['is_logged_in'] = false;
+            redirect('/marlin/20_tasks/task_15.php');
+        }
+    }else{
+        $_SESSION['error'] = 'Введите корректный почтовый адрес!';
+        redirect('/marlin/20_tasks/task_15.php');
+    }
+?>
